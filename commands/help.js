56548790a9b13a44a8637c109701e1fe450e75c0
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-21 19:08:07 +0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+const { MessageEmbed: o } = require('discord.js');
+
+module.exports = async (bot, message, args) => {
+
+ const embed = new o()
+ .setColor('RANDOM')
+ .setTitle('Help command for Chicken Store Bot!')
+ .setDescription(`Hello **${message.author.tag}** 
+}
