4df56cf91c00bcf99f5e2b725afc047c5d266d2c
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-22 20:34:08 +0700

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -17,14 +17,14 @@ message.guild.channels.create(`ticket-${message.author.id}`, {
  type: 'text',
  topic: 'Ketik `>end` untuk mengakhiri',
  permissionOverwrites: [{ id: message.guild.id, deny:["VIEW_CHANNEL", "SEND_MESSAGES"]}, { id: message.author.id, allow:["VIEW_CHANNEL", "SEND_MESSAGES"]}]
-}).then(ch => {
- bot.ticket.userID = message.author.id;
- bot.ticket.channelID = ch.id;
+}).then(async ch => {
+ 
+const data = await bot.ticket({ userID: message.author.id, channelID: ch.id });
 
- await bot.ticket.save().then(error => {
+ data.save().catch(error => console.log(error));
+
   message.reply(`➡ Ticket channel anda : <#${ch.id}`);
   ch.send({embed:{ color: 0xfca, title: 'Ticket Support', description: `Silahkan ketik keluhan atau apa saja tentang ~~pelayanan~~ kami disini!`, footer: { text: '© Chicken NodeJS' }}});
- }) // end save
 }) // end ch
 }); // findOne end
 } // exports end
