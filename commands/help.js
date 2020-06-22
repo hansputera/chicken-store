dbbcaa601cecab153bc843c627e0e74335eb72e2
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-22 08:02:08 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,5 +5,11 @@ module.exports = async (bot, message, args) => {
  const embed = new o()
  .setColor('RANDOM')
  .setTitle('Help command for Chicken Store Bot!')
- .setDescription(`Hello **${message.author.tag}** 
+ .setDescription(`Hello **${message.author.tag}** welcome to chicken store!`)
+ .setThumbnail(message.guild.iconURL())
+ .addField('📪 Customer', `\`order\`,\`ticket\``)
+ .addField('📊 General', `\`say\`,\`help\`,\`mcserver\``)
+ .setTimestamp();
+ 
+ message.channel.send(embed);
 }
