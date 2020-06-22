00743fca374f4eb36ebab4761d8147aaf4d8857a
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-22 20:27:44 +0700

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,36 +1,30 @@
 module.exports = async (bot, message, args) => {
- await bot.ticket.findOne({ userID: message.author.id }, async (err, res) => {
-   if (err) {
-    message.reply('Error telah terjadi silahkan hubungi staff untuk mengeceknya!');
-    console.log(err.message);
-   }
-   if (res) {
-    return message.channel.send('Ticket kamu sudah ada sebelumnya mohon gunakan yang lama dulu!');
-   }
-    
-    message.guild.channels.create(`ticket-${message.author.id}`, {
-    type: 'text',
-    permissionOverwrites: [
-    {
-     id: message.guild.id,
-     deny: ['VIEW_CHANNEL', 'SEND_MESSAGES']
-    },
-    {
-    id: message.author.id,
-    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
-    }
-    ]
-    }).then(ch => {
-     bot.ticket.userID = message.author.id;
-     bot.ticket.channelID = ch.id;
-     
-     bot.ticket.save().then(err => {
-      if (err) {
-       console.log(err.message);
-      }
-     ch.send({ embed: { color: 5921535, title: 'Ticket Support', description: 'Silahkan ketik keluhan atau apa saja disini yang anda butuhkan!', footer: { text: '© Copyright 2020 ChickenJS' }}});
-     message.reply('Your Ticket Support Channel: <#${ch.id}>`);
-     })
-    })
- })
-}
+
+
+
+await bot.ticket.findOne({ userID: message.author.id }, (err, res) => {
+
+if (err) {
+ console.log(err);
+ message.channel.send('_susuatu ada yang terjadi_ silahkan melapor pada admin yang tampan dan berlaku!');
+} // error end
+
+if (res) {
+ message.reply('Ticket kamu sudah tersedia sebelumnya, gunakan saja channel tersebut!');
+} // res end
+
+message.guild.channels.create(`ticket-${message.author.id}`, {
+ type: 'text',
+ topic: 'Ketik `>end` untuk mengakhiri',
+ permissionOverwrites: [{ id: message.guild.id, deny:["VIEW_CHANNEL", "SEND_MESSAGES"]}, { id: message.author.id, allow:["VIEW_CHANNEL", "SEND_MESSAGES"]}]
+}).then(ch => {
+ bot.ticket.userID = message.author.id;
+ bot.ticket.channelID = ch.id;
+
+ bot.ticket.save().then(error => {
+  message.reply(`➡ Ticket channel anda : <#${ch.id}`);
+  ch.send({embed:{ color: 0xfca, title: 'Ticket Support', description: `Silahkan ketik keluhan atau apa saja tentang ~~pelayanan~~ kami disini!`, footer: { text: '© Chicken NodeJS' }}});
+ }) // end save
+}) // end ch
+}); // findOne end
+} // exports end
